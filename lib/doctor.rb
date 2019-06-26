0ce36f668996414a4d4dd364ecfe627d6cffe6d0
Mark May <mamark90@gmail.com>
2019-06-26 21:18:30 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,17 +1,29 @@
 require 'pry'
 
 class Doctor
-  attr_accessor :name
+  attr_accessor :name, :patient, :date
 
   @@all = []
-  ]
+
   def initialize (name)
     @name = name
     @@all << self
   end
 
-  def .self.all
+  def self.all
     @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    appointments.map {|appointments| appointments.patient}
   end
 
 end
