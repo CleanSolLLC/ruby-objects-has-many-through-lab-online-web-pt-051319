fc2438391776cc1517b3e11ce642728db43584f9
Mark May <mamark90@gmail.com>
2019-06-26 20:50:01 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,2 +1,17 @@
+require 'pry'
+
 class Doctor
+  attr_accessor :name
+
+  @@all = []
+  ]
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def .self.all
+    @@all
+  end
+
 end
